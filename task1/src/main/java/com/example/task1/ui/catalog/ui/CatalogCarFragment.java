fbda82b2fdb2c399088ed1e41de14c10cd58b44c
AmdrewKornia <hairmanandrew@gmail.com>
2022-11-14 15:37:23 +0300

    corrected the code

--- task1/src/main/java/com/example/task1/ui/catalog/ui/CatalogCarFragment.java
+++ task1/src/main/java/com/example/task1/ui/catalog/ui/CatalogCarFragment.java
@@ -1,7 +1,6 @@
 package com.example.task1.ui.catalog.ui;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +17,6 @@ import com.example.task1.ui.catalog.vm.ViewModelsVehicle;
 
 import java.util.List;
 
-import timber.log.Timber;
-
 public class CatalogCarFragment extends Fragment {
 
     TextView login;
@@ -29,26 +26,30 @@ public class CatalogCarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_catalog_car,container,false);
+        View view = inflater.inflate(R.layout.fragment_catalog_car, container, false);
         login = view.findViewById(R.id.fc_login);
         recyclerView = view.findViewById(R.id.fc_list_vehicle);
         return view;
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         login.setText(CatalogCarFragmentArgs.fromBundle(getArguments()).getUserLogin());
         initUI();
     }
+
     private void initUI() {
         createCatalog();
         createAdapter();
     }
+
     private void createCatalog() {
         ViewModelsVehicle vehicle = new ViewModelsVehicle();
         vehicle.addVehicle();
         list = vehicle.getVehicleList();
     }
+
     private void createAdapter() {
         StateAdapter adapter = new StateAdapter(list, getContext(), getContext());
         recyclerView.setAdapter(adapter);
